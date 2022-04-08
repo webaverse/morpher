936c9672591f923c4ff9919a2a03c0356053f268
Avaer Kazmer <a@modules.io>
2022-04-07 23:42:36 -0400

    Rewrite index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
-import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+// import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 // import easing from './easing.js';
 import metaversefile from 'metaversefile';
 // import {getCaretAtPoint} from 'troika-three-text';
-const {useApp, useInternals, useGeometries, useMaterials, getAppByPhysicsId, useFrame, useActivate, useLoaders, usePhysics, useTextInternal, addTrackedApp, useDefaultModules, useCleanup} = metaversefile;
+const {useApp, useFrame, useActivate, useWear, usePhysics, useCleanup} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
@@ -16,21 +16,24 @@ export default e => {
   // const Text = useTextInternal();
 
   let morpher = null;
+  let head = null;
   // const speed = 0.03;
   // const angularSpeed = 0.02;
   (async () => {
     morpher = await metaversefile.createAppAsync({
-      start_url: `${baseUrl}morpher_mixed_v4_texta.glb`,
+      start_url: `${baseUrl}morpher_mixed_v6_texta_work.glb`,
     });
     // morpher.quaternion.copy(y180Quaternion);
     morpher.frustumCulled = false;
     app.add(morpher);
+
+    head = morpher.getObjectByName('Sphere028');
   })();
 
   // let running = false;
-  useFrame(({timestamp}) => {
+  /* useFrame(({timestamp}) => {
     
-  });
+  }); */
 
   /* let activateCb = null;
   let frameCb = null;
@@ -43,9 +46,7 @@ export default e => {
   });
 
   app.addEventListener('hit', e => {
-    /* // console.log('silk worm hit', e);
-
-    const {hitDirection} = e;
+    /* const {hitDirection} = e;
     const hitDirectionXZ = hitDirection.clone();
     hitDirectionXZ.y = 0;
     hitDirectionXZ.normalize();
@@ -56,11 +57,62 @@ export default e => {
 
     silkWormAction = hitAction(hitDirectionXZ, hitVelocity); */
   });
+  
+  useActivate(() => {
+    if (morpher) {
+      head.material.emissiveMap.offset.x = Math.floor(Math.random() * 8) / 8;
+      head.material.emissiveMap.offset.y = Math.floor(Math.random() * 8) / 8;
+
+      head.material.emissive.setHex(0x66bb6a);
+
+      // console.log('got glb', morpher.glb);
+    }
+  });
+
+  const animationMixers = [];
+  useFrame(({timestamp, timeDiff}) => {
+    if (morpher) {
+      const deltaSeconds = timeDiff / 1000;
+      for (const mixer of animationMixers) {
+        mixer.update(deltaSeconds);
+        morpher.updateMatrixWorld();
+      }
+    }
+  });
+
+  useWear(e => {
+    if (morpher) {
+      morpher.stop();
+
+      if (e.wear) {
+        morpher.traverse(o => {
+          if (o.isMesh) {
+            const {animations} = morpher.glb;
+            const animationName = 'watch_idle';
+            const animation = animations.find(a => a.name === animationName);
+            if (animation) {
+              const mixer = new THREE.AnimationMixer(o);
+              
+              const action = mixer.clipAction(animation);
+              action.play();
+
+              animationMixers.push(mixer);
+            }
+          }
+        });
+      } else {
+        for (const mixer of animationMixers) {
+          mixer.stopAllAction();
+        }
+        animationMixers.length = 0;
+      }
+    }
+  });
 
   const physicsIds = [];
   const physicsMaterial = new THREE.Vector3(0.5, 0.5, 1);
   const physicsObject = physics.addCapsuleGeometry(app.position, app.quaternion, 0.3, 0.2, physicsMaterial);
-  physicsObject.detached = true;
+  // physicsObject.detached = true;
   physicsIds.push(physicsObject);
   
   useCleanup(() => {
@@ -68,6 +120,8 @@ export default e => {
       physics.removeGeometry(physicsId);
     }
   });
+
+  app.getPhysicsObjects = () => physicsIds;
 
   return app;
 };
